696076c7304ba5f2acde0766f5e51f64093e1044
Dave Meehan <dave_meehan@replicated.co.uk>
2013-10-28 11:44:07 +0000

    Initial Commit

---
+++ RSErrorKit/RSErrorKit.h
@@ -0,0 +1,13 @@
+//
+//  RSErrorKit.h
+//  RSErrorKit
+//
+//  Created by Dave Meehan on 28/10/2013.
+//  Copyright (c) 2013 Replicated Solutions Limited. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RSErrorKit : NSObject
+
+@end
---
+++ RSErrorKit/RSErrorKit.m
@@ -0,0 +1,13 @@
+//
+//  RSErrorKit.m
+//  RSErrorKit
+//
+//  Created by Dave Meehan on 28/10/2013.
+//  Copyright (c) 2013 Replicated Solutions Limited. All rights reserved.
+//
+
+#import "RSErrorKit.h"
+
+@implementation RSErrorKit
+
+@end
